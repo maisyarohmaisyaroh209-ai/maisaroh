21cd0d94061569fa7b42830ee779001534d276fe
maisyarohmaisyaroh209-ai <maisyarohmaisyaroh209@gmail.com>
2025-10-23 10:14:10 +0700

    Add click event to greeting button

---
+++ scrip.js
@@ -0,0 +1,7 @@
+// 1. Pilih tombol berdasarkan ID-nya
+conts tombolsapa = document . getElelementById('sapaButton'):
+// 2. Tambahkan 'event listener' untuk aksi 'click'
+tombolsapa.addEventListener('click', function() {
+  // 3. Jalankan kode ini ketika tombol di-klik
+  alert('Halo! Terima kasih sudah berkunjung!');
+});
